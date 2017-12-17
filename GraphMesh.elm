8c591e6c71a8c1423feaf93272d7dda120ab9451
odf <olaf.delgado@gmail.com>
2017-12-17 14:21:34 +1100

    change graph field types and add some type aliases

--- GraphMesh.elm
+++ GraphMesh.elm
@@ -6,35 +6,49 @@ import WebGL
 import Renderer exposing (Vertex)
 
 
+type alias Adjacencies =
+    Array (List Int)
+
+
+type alias Embedding =
+    Array Vec3
+
+
 type alias Graph =
-    { adjacencies : List (List Int)
-    , positions : Maybe (List Vec3)
+    { adjacencies : Adjacencies
+    , positions : Maybe Embedding
     }
+
+
+type alias Embedder =
+    Adjacencies -> Embedding
 
 
 cube : Graph
 cube =
     { adjacencies =
-        [ [ 5, 4, 2 ]
-        , [ 6, 1, 3 ]
-        , [ 7, 2, 4 ]
-        , [ 8, 3, 1 ]
-        , [ 1, 6, 8 ]
-        , [ 2, 7, 5 ]
-        , [ 3, 8, 6 ]
-        , [ 4, 5, 7 ]
-        ]
-    , positions =
-        Just
-            [ (vec3 -1 -1 -1)
-            , (vec3 1 -1 -1)
-            , (vec3 1 1 -1)
-            , (vec3 -1 1 -1)
-            , (vec3 -1 -1 1)
-            , (vec3 1 -1 1)
-            , (vec3 1 1 1)
-            , (vec3 -1 1 1)
+        Array.fromList
+            [ [ 4, 3, 1 ]
+            , [ 5, 0, 2 ]
+            , [ 6, 1, 3 ]
+            , [ 7, 2, 0 ]
+            , [ 0, 5, 7 ]
+            , [ 1, 6, 4 ]
+            , [ 2, 7, 5 ]
+            , [ 3, 4, 6 ]
             ]
+    , positions =
+        Just <|
+            Array.fromList
+                [ (vec3 -1 -1 -1)
+                , (vec3 1 -1 -1)
+                , (vec3 1 1 -1)
+                , (vec3 -1 1 -1)
+                , (vec3 -1 -1 1)
+                , (vec3 1 -1 1)
+                , (vec3 1 1 1)
+                , (vec3 -1 1 1)
+                ]
     }
 
 
@@ -46,30 +60,28 @@ vertex pos =
     }
 
 
-positions : Graph -> List Vec3
-positions graph =
-    Maybe.withDefault [] graph.positions
-
-
 lines : Graph -> List ( Vertex, Vertex )
 lines graph =
     let
-        n =
-            List.length graph.adjacencies
-
-        ps =
-            Array.fromList (positions graph)
+        positions =
+            Maybe.withDefault Array.empty graph.positions
 
         edge p1 p2 =
             ( (vertex p1), (vertex p2) )
 
         getPos v =
-            Array.get (v - 1) ps
+            Array.get v positions
 
-        edges v adj =
+        edges ( v, adj ) =
             List.filterMap (\w -> Maybe.map2 edge (getPos v) (getPos w)) adj
     in
-        List.concat <| List.map2 edges (List.range 1 n) graph.adjacencies
+        List.concat <|
+            List.map edges (Array.toIndexedList graph.adjacencies)
+
+
+embed : Embedder -> Graph -> Graph
+embed embedder graph =
+    { graph | positions = Just (embedder graph.adjacencies) }
 
 
 mesh : WebGL.Mesh Renderer.Vertex
