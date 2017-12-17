c57a2c9880eeb5591f4c5c55aaa1098364ca8f1e
odf <olaf.delgado@gmail.com>
2017-12-17 12:58:16 +1100

    define a graph type with optional vertex positions

--- GraphMesh.elm
+++ GraphMesh.elm
@@ -6,17 +6,36 @@ import WebGL
 import Renderer exposing (Vertex)
 
 
-graph : List ( Vec3, List Int )
-graph =
-    [ ( (vec3 -1 -1 -1), [ 5, 4, 2 ] )
-    , ( (vec3 1 -1 -1), [ 6, 1, 3 ] )
-    , ( (vec3 1 1 -1), [ 7, 2, 4 ] )
-    , ( (vec3 -1 1 -1), [ 8, 3, 1 ] )
-    , ( (vec3 -1 -1 1), [ 1, 6, 8 ] )
-    , ( (vec3 1 -1 1), [ 2, 7, 5 ] )
-    , ( (vec3 1 1 1), [ 3, 8, 6 ] )
-    , ( (vec3 -1 1 1), [ 4, 5, 7 ] )
-    ]
+type alias Graph =
+    { adjacencies : List (List Int)
+    , positions : Maybe (List Vec3)
+    }
+
+
+cube : Graph
+cube =
+    { adjacencies =
+        [ [ 5, 4, 2 ]
+        , [ 6, 1, 3 ]
+        , [ 7, 2, 4 ]
+        , [ 8, 3, 1 ]
+        , [ 1, 6, 8 ]
+        , [ 2, 7, 5 ]
+        , [ 3, 8, 6 ]
+        , [ 4, 5, 7 ]
+        ]
+    , positions =
+        Just
+            [ (vec3 -1 -1 -1)
+            , (vec3 1 -1 -1)
+            , (vec3 1 1 -1)
+            , (vec3 -1 1 -1)
+            , (vec3 -1 -1 1)
+            , (vec3 1 -1 1)
+            , (vec3 1 1 1)
+            , (vec3 -1 1 1)
+            ]
+    }
 
 
 vertex : Vec3 -> Renderer.Vertex
@@ -27,27 +46,32 @@ vertex pos =
     }
 
 
-lines : List ( Vec3, List Int ) -> List ( Vertex, Vertex )
-lines items =
+positions : Graph -> List Vec3
+positions graph =
+    Maybe.withDefault [] graph.positions
+
+
+lines : Graph -> List ( Vertex, Vertex )
+lines graph =
     let
         n =
-            List.length items
+            List.length graph.adjacencies
 
-        positions =
-            Array.fromList <| List.map (\( v, _ ) -> v) items
+        ps =
+            Array.fromList (positions graph)
 
         edge p1 p2 =
             ( (vertex p1), (vertex p2) )
 
         getPos v =
-            Array.get (v - 1) positions
+            Array.get (v - 1) ps
 
-        edges v ( _, adj ) =
+        edges v adj =
             List.filterMap (\w -> Maybe.map2 edge (getPos v) (getPos w)) adj
     in
-        List.concat <| List.map2 edges (List.range 1 n) items
+        List.concat <| List.map2 edges (List.range 1 n) graph.adjacencies
 
 
 mesh : WebGL.Mesh Renderer.Vertex
 mesh =
-    WebGL.lines <| lines graph
+    WebGL.lines <| lines cube
